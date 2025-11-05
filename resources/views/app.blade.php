f005241770f46b633cb942db0d84e3f13abcf166
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2025-11-05 22:45:03 +0100

    Add Raleway font family to CSS and update global font settings in app.css. Remove external font link from app.blade.php for improved performance and consistency.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,10 +24,6 @@
         <!-- Favicon -->
         <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', 'resources/css/app.css'])
